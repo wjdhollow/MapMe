49e6b7c206d9a50c1fbaefe177d6dc3507b29435
Ambuj Punn <ambuj@ubuntu.(none)>
2012-03-02 23:24:19 -0800

    filtered dates

--- javascript/main.js
+++ javascript/main.js
@@ -36,12 +36,22 @@ $(document).ready(function() {
 
 	// Outputs all the user's friend's events to the console
 	window.friendsEvents = function() {
+	var currentTime = Math.round((new Date()).getTime() / 1000);
+	alert(currentTime);
            FB.api('/me/friends', function(response) {
               for(var i=0; i<response.data.length; i++) {
                  friendId = response.data[i].id; 
                  FB.api('/'+friendId+'/events', function(response) {
-                    for(var i=0; i<response.data.length; i++) {
-		       console.log('Friends Event: ' + response.data[i].name);
+                    for(var j=0; j<response.data.length; j++) {
+		 	var eventEndTimeString = response.data[j].end_time;
+			var d = new Date(eventEndTimeString);
+			var eventEndTime = Math.round(d.getTime() / 1000);
+			// filters it based on current time, so past events dont come up
+			if (currentTime <= eventEndTime) {
+		       		console.log('Friends Event: ' + response.data[j].name);
+				console.log('End time: ' + d);
+			}
+		  
 	            }
                  });
               } 
