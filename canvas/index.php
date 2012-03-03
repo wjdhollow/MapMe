a0fcc825fcd9a045ba40fe63bfe367a1c72d461a
William Holloway <wjdhollow@gmail.com>
2012-03-02 17:52:08 -0800

    changed index removed php-sdk dir and moved contents to src/

--- canvas/index.php
+++ canvas/index.php
@@ -1,70 +1,39 @@
 <?php
-// Path to PHP-SDK
-require '../php-sdk/facebook.php';
+  
+  // Path to PHP-SDK
+  include_once "../src/facebook.php";   
 
-$facebook = new Facebook(array(
-  'appId'  => ' 319369998112036',
-  'secret' => ' f4b4e8760670ae28f027d23f0b8f331e',
-));
+  $app_id = '101551916642221';   
+  $application_secret = 'c59f47a768fa5afae0213a451453daa3';   
+  $facebook = new Facebook(array(   'appId'  => $app_id,
+    'secret' => $application_secret,
+    'cookie' => true, // enable optional cookie support
+  ));
 
-// See if there is a user from a cookie
-$user = $facebook->getUser();
-
-if ($user) {
-  try {
-    // Proceed knowing you have a logged in user who's authenticated.
-    $user_profile = $facebook->api('/me');
-  } catch (FacebookApiException $e) {
-    echo '<pre>'.htmlspecialchars(print_r($e, true)).'</pre>';
-    $user = null;
+  if ($facebook->getSession())
+  {
+    $user = $facebook->getUser();
   }
-}
+  else
+  {
+    $loginUrl = "https://graph.facebook.com/oauth/authorize?type=user_agent&display=page&client_id=APPID
+    &redirect_uri=http://apps.facebook.com/CANVAS URL/
+    &scope=user_photos";
+    echo '';
+  }
+
+  $uid = $facebook->getUser(); // get the UID of the current user
+  $me = $facebook->api('/me/friends'); // access all the information of friends
+
+  echo " friends collage "
+
+  foreach($me['data'] as $frns)
+  {
+    // Display the picture of friends one by one
+    echo "";
+  }
 
 ?>
 <!DOCTYPE html>
-<html xmlns:fb="http://www.facebook.com/2008/fbml">
-<head>
-	<title>Facebook Application Local Development</title>
-	<link type="text/css" rel="stylesheet" href="css/reset.css">
-	<link type="text/css" rel="stylesheet" href="css/main.css">
-</head>
-  <body>
-	<div id="login">
-		<?php if (!$user) { ?>
-		  <fb:login-button></fb:login-button>
-		<?php } ?>
-	</div>
-	<div id="cont">
-		<?php if ($user): ?>
-		<img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
-		<p>Hello <?php echo $user_profile['name']; ?>!</p>
-
-		<?php else: ?>
-		<strong><em>You are not Connected.</em></strong>
-		<?php endif ?>
-	</div>
-    <div id="fb-root"></div>
-    <script>
-      window.fbAsyncInit = function() {
-        FB.init({
-          appId: '<?php echo $facebook->getAppID() ?>',
-          cookie: true,
-          xfbml: true,
-          oauth: true
-        });
-        FB.Event.subscribe('auth.login', function(response) {
-          window.location.reload();
-        });
-        FB.Event.subscribe('auth.logout', function(response) {
-          window.location.reload();
-        });
-      };
-      (function() {
-        var e = document.createElement('script'); e.async = true;
-        e.src = document.location.protocol +
-          '//connect.facebook.net/en_US/all.js';
-        document.getElementById('fb-root').appendChild(e);
-      }());
-    </script>
-  </body>
+<html>
 </html>
