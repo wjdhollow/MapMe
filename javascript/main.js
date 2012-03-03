d787775a3c567ebeee695b674b7d69b635666fcd
Jake Rabinowitz <jakerabinowitz@ubuntu.(none)>
2012-03-02 23:49:16 -0800

    probably bad

--- javascript/main.js
+++ javascript/main.js
@@ -29,7 +29,17 @@ $(document).ready(function() {
 	window.myEvents = function() {
 	   FB.api('/me/events', function(response) {
               for(var i=0; i<response.data.length; i++) {
-	         console.log('My Events: ' + response.data[i].name);
+		console.log("-------------EVENT------------");
+	         console.log('id: ' + response.data[i].id);
+	         console.log('owner: ' + response.data[i].owner);
+	         console.log('name: ' + response.data[i].name);
+	         console.log('description: ' + response.data[i].description);
+	         console.log('start_time: ' + response.data[i].start_time);
+	         console.log('end_time: ' + response.data[i].end_time);
+	         console.log('location: ' + response.data[i].location);
+	         console.log('venue: ' + response.data[i].venue);
+	         console.log('privacy: ' + response.data[i].privacy);
+	         console.log('updated_time: ' + response.data[i].updated_time);
 	      }
           });
 	};
@@ -41,14 +51,12 @@ $(document).ready(function() {
                  friendId = response.data[i].id; 
                  FB.api('/'+friendId+'/events', function(response) {
                     for(var i=0; i<response.data.length; i++) {
-		       console.log('Friends Event: ' + response.data[i].name);
+		       console.log(response.data[i].description + ': ' + response.data[i].name);
 	            }
                  });
               } 
            });
 	};
-
-
 
 });
 
