94ee1f89100de04824bbc4d08813eb71968a46fe
William Holloway <wjdhollow@gmail.com>
2012-03-03 01:10:31 -0800

    added User model

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,24 @@
 
 $(document).ready(function() {
 	
+  User = Backbone.Model.extend({
+    defaults: {
+      name: 'nobody',
+      id: 123,
+      friends: []
+    },
+    
+    befriend: function ( newFriend ) {
+      var friend_array = this.get('friends');
+      console.log('befriending ' + newFriend.get('name'));
+      friend_array.push( newFriend );
+      this.set({friends: friend_array});
+    }
+
+  });
+
+  var myUser = new User;
+
 	window.fbAsyncInit = function() {
    		FB.init({
       		appId      : '319369998112036',
@@ -12,9 +30,24 @@ $(document).ready(function() {
 
       FB.getLoginStatus(function (response) {
         if (response.status === 'connected') {
-          FB.api('/me', function(user) {
-            alert('user name ' + user.name);
+
+          FB.api('/me', function(response) {
+            $('#image').attr('src','http://graph.facebook.com/' + response.id + '/picture');            
+            $('#name').text(response.name);
+            myUser.set({name: response.name, id: response.id});
           });
+
+          FB.api('/me/friends', function(response) {
+            for (var i = 0; i < response.data.length; i++) {
+              friendId = response.data[i].id;
+              friendName = response.data[i].name;
+              friend = new User;
+              friend.set({ name : friendName, id : friendId});
+              // console.log('friend setup ' + friendName + ' ' + friendId);
+              myUser.befriend(friend);
+            }
+          });
+          
         }
       });
   	};
@@ -34,7 +67,7 @@ $(document).ready(function() {
 	};
 
 	// Outputs all of the user's events to the console.
-	var myEvents = function() {
+	window.myEvents = function() {
 	   FB.api('/me/events', function(response) {
               for(var i=0; i<response.data.length; i++) {
 	         console.log('My Events: ' + response.data[i].name);
@@ -55,6 +88,8 @@ $(document).ready(function() {
       } 
     });
 	};
+
+
 });
 
 
