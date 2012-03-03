4f68878fe3903790ebcab9b3da5169d62abb9069
William Holloway <wjdhollow@gmail.com>
2012-03-02 20:59:17 -0800

    cleaned up index and main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,29 @@
+
 $(document).ready(function() {
-   // put all your jQuery goodness in here.
- });
+	
+	window.fbAsyncInit = function() {
+   		FB.init({
+      		appId      : '319369998112036',
+      		status     : true, 
+      		cookie     : true,
+      		xfbml      : true,
+      		oauth      : true,
+    	});
+  	};
+
+  	(function(d){
+    	var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+    	js = d.createElement('script'); js.id = id; js.async = true;
+    	js.src = "//connect.facebook.net/en_US/all.js";
+    	d.getElementsByTagName('head')[0].appendChild(js);
+  	}(document));
+   	// put all your jQuery goodness in here.
+
+	window.showName = function()  {
+		FB.api('/me', function(response) {
+   			alert(response.name);
+   		});
+	};
+});
+
+
